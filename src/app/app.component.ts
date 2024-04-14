42d347ff00298fc38f76000e0242aa77197e7e9b
Dionisiohenriq <henriquedionisio.dionisioferre@gmail.com>
2024-04-13 23:51:29 -0300

    new views and components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+import { HomeComponent } from './views/home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
